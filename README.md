4828daf0acd08b89ba34ea35f727741390b38194
neil patel <neil@myindianweddingapp.com>
2014-04-18 19:47:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+== README
+
+Odot is a simple To-do List anyone can use 
+
+* Ruby version 2.0 
+
+* Rails version 4.0.0
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = 'c8a458af6aea0582a622f4f52b6a3ccc25008511ad8dbe45fea4521897189618de6e51b7ad04ac2bfb4592c0177e4ec2c5fff8a7518033ca7c1df36f46bc995d'
